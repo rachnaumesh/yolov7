a30a5bab8d2ceb12427a8e0aee4a0c9cceb29de6
Rachna Umesh <rachnaumesh@Rachnas-MacBook-Pro.local>
2024-02-01 15:31:46 +0530

    direct segmentation mask inference

---
+++ seg/segment/segment.py
@@ -0,0 +1,166 @@
+import argparse
+import os
+import platform
+import sys
+from pathlib import Path
+
+import torch
+import torch.backends.cudnn as cudnn
+
+FILE = Path(__file__).resolve()  # noqa: E402
+ROOT = FILE.parents[1]  # YOLOv5 root directory # noqa: E402
+if str(ROOT) not in sys.path:  # noqa: E402
+    sys.path.append(str(ROOT))  # add ROOT to PATH # noqa: E402
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative # noqa: E402
+
+from models.common import DetectMultiBackend
+from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
+from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+                           increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
+from utils.plots import Annotator, colors, save_one_box
+from utils.segment.general import process_mask, scale_masks
+from utils.segment.plots import plot_masks
+from utils.torch_utils import select_device, smart_inference_mode
+from utils.augmentations import letterbox
+import numpy as np
+
+
+@smart_inference_mode()
+def run_segment(
+        weights=ROOT / 'yolov5s-seg.pt',  # model.pt path(s)
+        img_array=None,  # numpy array for image
+        data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
+        imgsz=(640, 640),  # inference size (height, width)
+        conf_thres=0.25,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        max_det=1000,  # maximum detections per image
+        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        classes=None,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms=False,  # class-agnostic NMS
+        augment=False,  # augmented inference
+        visualize=False,  # visualize features
+        update=False,  # update all models
+        half=False,  # use FP16 half-precision inference
+        dnn=False,  # use OpenCV DNN for ONNX inference
+):
+
+
+    # Load model
+    device = select_device(device)
+    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
+    stride, names, pt = model.stride, model.names, model.pt
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Data preprocessing
+    im0 = img_array
+    im = letterbox(im0, imgsz, stride=stride, auto=pt)[0]  # padded resize
+    im = im.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+    im = np.ascontiguousarray(im)  # contiguous
+    bs = 1  # batch_size
+    vid_path, vid_writer = [None] * bs, [None] * bs
+
+    # Run inference
+    model.warmup(imgsz=(1 if pt else bs, 3, *imgsz))  # warmup
+    seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+#     for path, im, im0s, vid_cap, s in dataset:
+    with dt[0]:
+        im = torch.from_numpy(im).to(device)
+        im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+        im /= 255  # 0 - 255 to 0.0 - 1.0
+        if len(im.shape) == 3:
+            im = im[None]  # expand for batch dim
+
+        # Inference
+        with dt[1]:
+            visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+            pred, out = model(im, augment=augment, visualize=visualize)
+            proto = out[1]
+
+        # NMS
+        with dt[2]:
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det, nm=32)
+
+        # Second-stage classifier (optional)
+        # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            seen += 1
+            im0 = im0.copy()
+           
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            annotator = Annotator(im0, line_width=3, example=str(names))
+            if len(det):
+                masks = process_mask(proto[i], det[:, 6:], det[:, :4], im.shape[2:], upsample=True)  # HWC
+
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, 5].unique():
+                    n = (det[:, 5] == c).sum()  # detections per class
+
+                # Mask plotting ----------------------------------------------------------------------------------------
+                mcolors = [colors(int(cls), True) for cls in det[:, 5]]
+                im_masks = plot_masks(im[i], masks, mcolors)  # image with masks shape(imh,imw,3)
+                annotator.im = scale_masks(im.shape[2:], im_masks, im0.shape)  # scale to original h, w
+                # Mask plotting ----------------------------------------------------------------------------------------
+                
+                # Write results
+                seg_boxes = []
+                conf_scores = []
+                for *xyxy, conf, cls in reversed(det[:, :6]):
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                    line = (cls, *xywh)  # label format
+                    # Convert each element to a PyTorch tensor and move it to CPU
+                    line_tensor = tuple(torch.tensor(elem).cpu().numpy() if isinstance(elem, torch.Tensor) else elem for elem in line)
+                    seg_boxes.append(np.array(line_tensor))
+                    conf_scores.append(conf)
+                
+                # print(f'number of boxes {len(det[:, :6])} and masks {len(masks)}')
+#                 print(f'masks are : {masks.detach().cpu().numpy()}')
+#                 print(f'boxes are : {seg_boxes}')
+
+        # Print time (inference-only)
+        LOGGER.info(f"{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+
+    # Print results
+    t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
+    # LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
+    if update:
+        strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
+        
+    return masks, seg_boxes, conf_scores
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s-seg.pt', help='model path(s)')
+    parser.add_argument('--img-array', type=np.ndarray, help='image numpy array')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--visualize', action='store_true', help='visualize features')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    opt = parser.parse_args()
+    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
+    print_args(vars(opt))
+    return opt
+
+
+def main(opt):
+    check_requirements(exclude=('tensorboard', 'thop'))
+    run(**vars(opt))
+
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
